a5eb265eac27c5971d25f828ff5bfd436db959d3
Dico Diaz <dicodiaz@gmail.com>
2023-08-26 17:07:09 -0500

    Solve day 9 - part 2

--- 2022/day_9_rope_bridge.rb
+++ 2022/day_9_rope_bridge.rb
@@ -3,9 +3,8 @@ lines = File.read('2022/Kotlin/src/main/inputs/day9.txt').lines(chomp: true)
 class Rope
   attr_reader :visited
 
-  def initialize
-    @head = [0, 0]
-    @tail = [0, 0]
+  def initialize(knots_count)
+    @knots = Array.new(knots_count, [0, 0])
     @visited = Set[[0, 0]]
     @directions_to_motion = { 'L' => [0, -1], 'R' => [0, 1], 'U' => [-1, 0], 'D' => [1, 0] }
     @adjacency_directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 0], [0, 1], [1, -1], [1, 0], [1, 1]]
@@ -14,26 +13,51 @@ class Rope
   def move(direction, steps)
     motion = @directions_to_motion[direction]
     steps.times do
-      new_head = [@head[0] + motion[0], @head[1] + motion[1]]
-      unless tail_adjacent_to?(new_head)
-        @tail = @head
-        @visited.add(@tail)
+      @knots.map!.with_index do |knot, i|
+        next [knot[0] + motion[0], knot[1] + motion[1]] if i.zero?
+        next knot if adjacent?(knot, @knots[i - 1])
+
+        prev_knot = @knots[i - 1]
+        row_diff = prev_knot[0] - knot[0]
+        col_diff = prev_knot[1] - knot[1]
+        new_knot = [knot[0] + (row_diff <=> 0), knot[1] + (col_diff <=> 0)]
+        @visited.add(new_knot) if i == @knots.length - 1
+        new_knot
       end
-      @head = new_head
     end
+  end
+
+  def draw
+    min_row, max_row = @knots.minmax { |knot1, knot2| knot1[0] <=> knot2[0] }.map(&:first)
+    min_col, max_col = @knots.minmax { |knot1, knot2| knot1[1] <=> knot2[1] }.map(&:last)
+    grid = Array.new(max_row - min_row + 1) { Array.new(max_col - min_col + 1, '.') }
+    @knots.to_enum.with_index.reverse_each { |knot, i| grid[knot[0] - min_row][knot[1] - min_col] = i }
+    print grid.map(&:join).join("\n").concat("\n")
   end
 
   private
 
-  def tail_adjacent_to?(new_head)
-    @adjacency_directions.any? { |direction| new_head == [@tail[0] + direction[0], @tail[1] + direction[1]] }
+  def adjacent?(knot1, knot2)
+    @adjacency_directions.any? { |direction| knot2 == [knot1[0] + direction[0], knot1[1] + direction[1]] }
   end
 end
 
-rope = Rope.new
+rope1 = Rope.new(2)
 lines.each do |motion_str|
   direction, steps = motion_str.split
   steps = steps.to_i
-  rope.move(direction, steps)
+  rope1.move(direction, steps)
 end
-p rope.visited.length
+p rope1.visited.length
+
+# part 2
+rope2 = Rope.new(10)
+lines.each_with_index do |motion_str, _i|
+  direction, steps = motion_str.split
+  steps = steps.to_i
+  rope2.move(direction, steps)
+  # p [i, '---', motion_str, '---']
+  # rope2.draw
+  # break if i == 18
+end
+p rope2.visited.length
